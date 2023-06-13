5d5dca54dcef1de4a421c6d72a150a22f9de4dc1
Bayero-abdul <bayeroa65@gmail.com>
2023-06-13 17:16:23 +0100

    Solved NSTEPS

--- Spoj/Number Steps.cpp
+++ Spoj/Number Steps.cpp
@@ -12,12 +12,12 @@ int main() {
 	ll x,y;
 	while(t--){
 		cin >> x >> y;
-		if (x != y || x != y + 2)
-			cout << "No number" << endl;
-		else if (x % 2 == 0)
+		if (x != y && x != y + 2)
+			cout << "No Number" << endl;
+		else if (x % 2 == 0 && y % 2 == 0)
 			cout << x + y << endl;
 		else
-			cout << x + y -1 << endl;
+			cout << x + y - 1 << endl;
 	}
 	return 0;
 }
